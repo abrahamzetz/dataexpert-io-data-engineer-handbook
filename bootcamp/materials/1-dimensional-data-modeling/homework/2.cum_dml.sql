a0a3aa99bfebfeebd8fecc842c25c1368c06f79d
Abraham Setiawan <abraham.setiawan@solita.fi>
2025-01-09 15:44:54 +0100

    modifying cum_dml to add all movies over the years

--- bootcamp/materials/1-dimensional-data-modeling/homework/2.cum_dml.sql
+++ bootcamp/materials/1-dimensional-data-modeling/homework/2.cum_dml.sql
@@ -19,21 +19,16 @@ select distinct
     COALESCE(ly.actor, ty.actor) as actor,
     COALESCE(ly.actorid, ty.actorid) as actorid,
     cy.current_year as year,
-    case when ty.year is not null then
-        array_agg(row(ty.film, ty.votes, ty.rating, ty.filmid)::films)
-    else ly.films
-    end as films,
-    case when ty.year is not null then
-        case when avg(ty.rating) > 8 then 'star'::quality_class
-             when avg(ty.rating) > 7 then 'good'::quality_class
-             when avg(ty.rating) > 6 then 'average'::quality_class
-             else 'bad'::quality_class
-        end
-    else ly.quality_class
+    ly.films || array_agg(row(ty.film, ty.votes, ty.rating, ty.filmid)::films) as films,
+    case 
+        when avg(ty.rating) > 8 then 'star'::quality_class
+        when avg(ty.rating) > 7 then 'good'::quality_class
+        when avg(ty.rating) > 6 then 'average'::quality_class
+        else 'bad'::quality_class
     end as quality_class,
     ty.year is not null as is_active
 from last_year ly
 full outer join this_year ty
     on ly.actorid = ty.actorid,
 current_year cy
-group by 1, 2, 3, 6, ly.films, ly.quality_class;
+group by 1, 2, 3, 6, ly.films;
